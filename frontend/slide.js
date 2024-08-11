65f36467d5689d3412f0d92193d79df7368a1382
S18 <63715649+Siddhantshetty@users.noreply.github.com>
2024-08-11 16:53:53 +0530

    Create slide.js

---
+++ frontend/slide.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var slides = document.querySelectorAll(".slide");
+    var captions = document.querySelectorAll(".caption");
+  
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].addEventListener("mouseover", showCaption);
+      slides[i].addEventListener("mouseout", hideCaption);
+    }
+  
+    function showCaption() {
+      var caption = this.querySelector(".caption");
+      caption.style.display = "block";
+    }
+  
+    function hideCaption() {
+      var caption = this.querySelector(".caption");
+      caption.style.display = "none";
+    }
+  });
+  function blinkImage() {
+    var image = document.getElementById('myImage');
+    image.classList.toggle('blink');
+  }
+
+  setInterval(blinkImage, 500);
+
+  function blinkWord() {
+    var word = document.getElementById('myWord');
+    word.classList.toggle('blink');
+  }
+
+  setInterval(blinkWord, 1000);
+  
